98e287c38103feb60c532478bbdf84e6a347c087
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-07-30 20:50:30 +0100

    Create Jailbreak.lua

---
+++ Jailbreak.lua
@@ -0,0 +1,31 @@
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("ReeqyHub ", "JJsploit is the best!", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+ 
+
+
+
+ 
+local Maintab = win:Tab("Jailbreak", "http://www.roblox.com/asset/?id=6023426915")
+
+
+
+Maintab:Button("Jailbreak", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+
+    Flux:Notification("Loading..", "Alright")
+end)
+
+
+
+
+
+
+
+
+
+Maintab:Button("Jailbreak", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))()
+
+    Flux:Notification("Loading..", "Alright")
+end)
