0b533517abad1bddc2e30b9f3fc3167545b7d0fa
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-07-31 04:18:13 +0100

    Create Phantom.lua

---
+++ Phantom.lua
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+local win = Flux:Window("ReeqyHub ", "JJsploit is the best!", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+ 
+
+
+
+ 
+local Maintab = win:Tab("Phantom Forces", "http://www.roblox.com/asset/?id=6023426915")
+
+
+
+Maintab:Button("Phantom Forces Aimbot", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Herrtt/Phantom-Forces-Cheat/main/Main.lua", true))()
+
+    Flux:Notification("Loading..", "Alright")
+end)
+
+
+
+
+
+
+
+
+
+
