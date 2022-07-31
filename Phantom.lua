4716dfe6d241c643ea6a3b287c1b23ba4b070307
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-07-31 05:57:18 +0100

    Update Phantom.lua

--- Phantom.lua
+++ Phantom.lua
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 
 local win = Flux:Window("ReeqyHub ", "JJsploit is the best!", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
@@ -12,14 +6,11 @@ local win = Flux:Window("ReeqyHub ", "JJsploit is the best!", Color3.fromRGB(255
 
 
  
-local Maintab = win:Tab("Phantom Forces", "http://www.roblox.com/asset/?id=6023426915")
+local Maintab = win:Tab("Phantom", "http://www.roblox.com/asset/?id=6023426915")
 
 
 
-
-
-
-Maintab:Button("Strawhook", "This function may not work sometimes and you can get Kicked.", function()
+Maintab:Button("Phantom Forces", "This function may not work sometimes and you can get Kicked.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua", true))()
 
     Flux:Notification("Loading..", "Alright")
@@ -30,8 +21,3 @@ end)
 
 
 
-
-
-
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua", true))()
